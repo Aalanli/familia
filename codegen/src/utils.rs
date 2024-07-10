efb306a4b1609224140d2ebe3fe1981019ce93c1
AL <allanlin666666@gmail.com>
2024-07-10 18:25:11 -0400

    remove unused

--- codegen/src/utils.rs
+++ codegen/src/utils.rs
@@ -1,20 +1,4 @@
 use std::process::Command;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref GPP: String = find_gpp();
-}
-
-fn find_gpp() -> String {
-    let output = Command::new("which")
-        .arg("g++")
-        .output()
-        .expect("failed to execute process");
-    assert!(output.status.success());
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    assert!(!stdout.is_empty());
-    stdout
-}
 
 pub fn object_to_executable(object_file: &str, executable_file: &str) {
     assert!(std::path::Path::new(object_file).exists());
@@ -28,9 +12,3 @@ pub fn object_to_executable(object_file: &str, executable_file: &str) {
     assert!(output.status.success());
 }
 
-#[test]
-fn test_which() {
-    let gpp = find_gpp();
-    assert!(!gpp.is_empty());
-    println!("{}", gpp);
-}
